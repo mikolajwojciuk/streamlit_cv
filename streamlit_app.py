9a1db424ff88011d51790fc95354dfb82b3d83a5
mikolajwojciuk <56088389+mikolajwojciuk@users.noreply.github.com>
2023-09-07 18:50:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,7 @@
 import streamlit as st
-import logging
 
-print("Importing openai embeddings")
 from langchain.embeddings.openai import OpenAIEmbeddings
-logging.info("Importing text splitter")
 from langchain.text_splitter import CharacterTextSplitter
-logging.info("Importings FAISS")
 from langchain.vectorstores import FAISS
-logging.info("Importing text loader")
 from langchain.document_loaders import TextLoader
-st.write("Hello, it's Mikołaj!")
+st.title("Hello, it's Mikołaj!")
