e6ab2d2ec5e5a0d70895ea9ca1b1189f439b880d
mikolajwojciuk <56088389+mikolajwojciuk@users.noreply.github.com>
2023-09-07 18:51:45 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.document_loaders import TextLoader
-st.title("Hello, it's Mikołaj!")
+st.header("Hello, it's Mikołaj!")
