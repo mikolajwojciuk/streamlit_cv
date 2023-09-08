1c1ec90d7189da22d09687e21831f350af9dfed0
mikolajwojciuk <56088389+mikolajwojciuk@users.noreply.github.com>
2023-09-08 14:43:12 +0200

    qa chain

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,39 @@
 import streamlit as st
-
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
-from langchain.document_loaders import TextLoader
-
-
-import streamlit as st
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
 import time
+import os
 
 st.title("Hi, I am here to tell You about Mikołaj. Go ahead and ask me some questions!")
 
+
+
+if 'faiss_db' not in st.session_state:
+    st.session_state.faiss_db = FAISS.load_local('faiss_index', OpenAIEmbeddings())
+
+if "qa_chain" not in st.session_state:
+    st.session_state.qa_chain = load_qa_chain(llm=OpenAI(model_name='gpt-3.5-turbo',temperature=0.1),chain_type='stuff')
 
 if "user_name" not in st.session_state:
     st.session_state.user_name = ""
 
 if "name_available" not in st.session_state:
-    st.session_state.name_available = False
+    st.session_state.name_available = True
 
 text_input_container = st.empty()
 text_input_container.text_input(placeholder='Your name',label="Before we begin, would You mind introducing Yourself?", key="user_name")
 if st.session_state.user_name != "":
     text_input_container.empty()
-    with st.empty():
-        st.write(f"Thanks!")
-        time.sleep(1)
-        st.empty()
-
-
+    if st.session_state.name_available:
+        st.session_state.name_available = False
+        with st.empty():
+            st.write(f"Thanks!")
+            time.sleep(1)
+            st.empty()
+        
 if st.session_state.user_name:
-    st.title("Echo Bot")
     # Initialize chat history
     if "messages" not in st.session_state:
         st.session_state.messages = []
@@ -38,12 +42,15 @@ if st.session_state.user_name:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
     # React to user input
-    if prompt := st.chat_input("What is up?"):
+    if query := st.chat_input("Question"):
         # Display user message in chat message container
-        st.chat_message("user").markdown(prompt)
+        st.chat_message("user").markdown(query)
         # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        response = f"Echo: {prompt}"
+        st.session_state.messages.append({"role": "user", "content": query})
+        #response = f"Echo: {prompt}"
+        #Generate response
+        relevant_docs = st.session_state.faiss_db.similarity_search(query)
+        response = st.session_state.qa_chain.run(input_documents=relevant_docs, question = query)
         # Display assistant response in chat message container
         with st.chat_message("assistant"):
             st.markdown(response)
