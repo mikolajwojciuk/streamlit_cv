07ca9d1a4b0d5466ae7508ac98c722c48f13cc46
mikolajwojciuk <56088389+mikolajwojciuk@users.noreply.github.com>
2023-10-04 22:34:46 +0200

    'about'

--- pages/1_About.py
+++ pages/1_About.py
@@ -7,9 +7,11 @@ st.sidebar.write(
     unsafe_allow_html=True,
 )
 
+
 st.header("About me")
 st.write(
-    """I have always been fascinated by the wonders of technology that surround us every day. From simple electronic circuits to complex algorithms that help locate me on the internet. Thanks to diligence, perseverance, and a constant questioning mindset, today I not only understand how they work but also actively participate in their creation.
+    """ Hi! It's Mikołaj here, the real one :)
+    I have always been fascinated by the wonders of technology that surround us every day. From simple electronic circuits to complex algorithms that help locate me on the internet. Thanks to diligence, perseverance, and a constant questioning mindset, today I not only understand how they work but also actively participate in their creation.
 
 Through various interests and passions, I have been able to earn the title of an engineer. Through experience, I have come to understand what it means to be an engineer. I am not afraid to challenge things I do not understand and get excited when the opportunity arises to learn something new.
 
@@ -17,4 +19,16 @@ Numerous experiences allow me to easily navigate previously unknown situations. 
 
 
 Technologies I am familiar with include: TensorFlow, PyTorch, Numpy, Pandas, Huggingface ecosystem, OpenCV"""
+)
+
+
+st.header("About this demo")
+st.write(
+    """This demo was originally created as my playground for experiments with LangChain. 
+    Currently, it uses LangChain and FAISS to tell the users a bit about myself as an addition to LinkedIn profile or CV. 
+    It is built with Streamlit as it provides appealing interface and offers broad range of possibilities when it comes to presenting the data and experiments outputs.
+
+    The data processing relies on finding similar documents among those previously prepared by me, containing answers to typical 'recruitment' questions. The discovered documents are then used to generate responses using the OpenAI API.
+
+"""
 )
