abbcb5b54f7676fe1dc37372c21c86130db7adca
mikolajwojciuk <56088389+mikolajwojciuk@users.noreply.github.com>
2023-10-04 21:59:19 +0200

    'test7'

--- pages/1_About.py
+++ pages/1_About.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="About", page_icon="🙋🏻‍♂️")
 
 st.sidebar.write(
-    """<div style="width:100%;text-align:center;"><a href="www.linkedin.com/in/mikołaj-wojciuk-72956a20b" style="float:center"><img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" width="22px"></img></a></div>""",
+    """<div style="width:100%;text-align:center;"><a href="https://www.linkedin.com/in/mikołaj-wojciuk-72956a20b" style="float:center"><img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" width="22px"></img></a></div>""",
     unsafe_allow_html=True,
 )
 
