3422673b5865f5f6ae170f4ce5ca62f1e3948191
mikolajwojciuk <56088389+mikolajwojciuk@users.noreply.github.com>
2023-10-04 21:44:15 +0200

    'test4'

--- pages/1_About.py
+++ pages/1_About.py
@@ -2,16 +2,9 @@ import streamlit as st
 
 st.set_page_config(page_title="About", page_icon="🙋🏻‍♂️")
 
-# st.sidebar.write(
-#    """<div style="width:100%;text-align:center;"><a href="www.linkedin.com/in/mikołaj-wojciuk-72956a20b" style="float:center"><img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" width="22px"></img></a></div>""",
-#    unsafe_allow_html=True,
-# )
+st.sidebar.write(
+    """<div style="width:100%;text-align:center;"><a href="www.linkedin.com/in/mikołaj-wojciuk-72956a20b" style="float:center"><img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" width="22px"></img></a></div>""",
+    unsafe_allow_html=True,
+)
 
-st.sidebar.markdown(
-    "[![Title]('https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg')]('www.linkedin.com/in/mikołaj-wojciuk-72956a20b')"
-)
-_, column2, _ = st.columns(3)
-column2.markdown(
-    "[![Title]('https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg')]('www.linkedin.com/in/mikołaj-wojciuk-72956a20b')"
-)
 st.header("About me")
